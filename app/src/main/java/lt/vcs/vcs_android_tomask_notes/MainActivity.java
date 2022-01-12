41dfe70eefc7329f36a4e5cf69bd4014c7e0065a
Tomas <tomas.kubilius.93@gmail.com>
2022-01-12 16:11:01 +0200

    setup listView

--- app/src/main/java/lt/vcs/vcs_android_tomask_notes/MainActivity.java
+++ app/src/main/java/lt/vcs/vcs_android_tomask_notes/MainActivity.java
@@ -3,6 +3,12 @@ package lt.vcs.vcs_android_tomask_notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> notes=new ArrayList<>();
+
+        notes.add("1-Name1-Content1");
+        notes.add("2-Name2-Content2");
+        notes.add("3-Name3-Content3");
+        notes.add("4-Name4-Content4");
+        notes.add("5-Name5-Content5");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        ListView listView= findViewById(R.id.listView);
+        listView.setAdapter(arrayAdapter);
+
+
     }
 }
