2881baa769207bea2eaea68a9974dd0738af8a0d
Tomas <tomas.kubilius.93@gmail.com>
2022-01-12 17:18:45 +0200

    Creating Note.java

---
+++ app/src/main/java/lt/vcs/vcs_android_tomask_notes/Note.java
@@ -0,0 +1,55 @@
+package lt.vcs.vcs_android_tomask_notes;
+
+import java.time.LocalDateTime;
+
+public class Note {
+
+    private int id;
+    private String name;
+    private String content;
+    private LocalDateTime creationDate;
+    private LocalDateTime updateDate;
+
+    public Note(int id, String name, String content) {
+
+        this.id = id;
+        this.name = name;
+        this.content = content;
+        this.creationDate = LocalDateTime.now();
+        this.updateDate = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
