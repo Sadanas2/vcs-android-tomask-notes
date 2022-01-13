088eec88717fe7f93b9f2ced5d62016b5f499203
Tomas <tomas.kubilius.93@gmail.com>
2022-01-14 00:30:17 +0200

    Open NotesDetails from MainActivity

--- app/src/main/java/lt/vcs/vcs_android_tomask_notes/MainActivity.java
+++ app/src/main/java/lt/vcs/vcs_android_tomask_notes/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -63,7 +64,10 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i(TAG, "onItemClick: " + notes.get(position).getId());
+//                Log.i(TAG, "onItemClick: " + notes.get(position).getId());
+
+                Intent intent= new Intent(MainActivity.this, NoteDetails.class);
+                startActivity(intent);
             }
         });
     }
